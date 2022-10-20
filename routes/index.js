7582d0c4286ef1afca87394f67e9939b620a1df9
mhamds <113810218+mhamds@users.noreply.github.com>
2022-10-20 01:38:16 -0400

    update routes

--- routes/index.js
+++ routes/index.js
@@ -13,35 +13,35 @@ app.set('view engine', 'ejs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/home');
+  res.render('home');
 });
 
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('pages/home');
+  res.render('home');
 });
 
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('pages/about');
+  res.render('about');
 });
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-  res.render('pages/projects');
+  res.render('projects');
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('pages/services');
+  res.render('services');
 });
 
 
 /* GET Contact us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('pages/contact');
+  res.render('contact');
 });
 
 module.exports = router;
