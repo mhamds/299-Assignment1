65cb1c745712a160ff28dcaf351095a97912df93
mhamds <113810218+mhamds@users.noreply.github.com>
2022-10-20 12:37:43 -0400

    last route commit

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,6 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-// router.get('/', (req, res) => {
-//   res.render('index');
-// });
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('home', { title: 'Home' });
-// });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,41 +16,23 @@ router.get('/home', function(req, res, next) {
   res.render('Pages/home');
 });
 
-/* GET home page. */
-// router.get('/home', function(req, res, next) {
-//   res.render('Pages/Home', { title: 'Home' });
-// });
-
 
 /* GET About page. */
  router.get('/about', function(req, res, next) {
   res.render('Pages/about');
 });
 
-/* GET About page. */
-// router.get('/about', function(req, res, next) {
-//   res.render('Pages/About', { title: 'About' });
-// });
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
   res.render('Pages/projects');
 });
 
-/* GET Projects page. */
-// router.get('/projects', function(req, res, next) {
-//   res.render('Pages/Projects', { title: 'Projects' });
-// });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
   res.render('Pages/services');
 });
-
-/* GET Services page. */
-// router.get('/services', function(req, res, next) {
-//   res.render('Pages/Services', { title: 'Services' });
-// });
 
 
 /* GET Contact us page. */
@@ -66,9 +40,6 @@ router.get('/contact', function(req, res, next) {
   res.render('Pages/contact');
 });
 
-/* GET Contact page. */
-// router.get('/contact', function(req, res, next) {
-//   res.render('Pages/Contact', { title: 'contact' });
-// });
+
 
 module.exports = router;
