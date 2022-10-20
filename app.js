305976a001b2ce56f000a173d118371057715323
mhamds <113810218+mhamds@users.noreply.github.com>
2022-10-20 02:02:24 -0400

    updated index with html view

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ let usersRouter = require('./routes/users');
 let app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');// express -e configered view engine to ejs
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
