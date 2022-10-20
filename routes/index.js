6eb8c8a43daceba537fc0252ac12b2b09234c599
mhamds <113810218+mhamds@users.noreply.github.com>
2022-10-20 11:37:53 -0400

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,46 +4,71 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-router.get('/', (req, res) => {
-  res.render('index');
-});
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Home' });
+// router.get('/', (req, res) => {
+//   res.render('index');
 // });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('./pages/home');
+  res.render('index', { title: 'Home' });
 });
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('./pages/home');
+// });
 
 
 /* GET home page. */
+// router.get('/home', function(req, res, next) {
+//   res.render('./pages/home');
+// });
+
+/* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('./pages/home');
+  res.render('index', { title: 'Home' });
 });
 
 
 /* GET About page. */
+// router.get('/about', function(req, res, next) {
+//   res.render('./pages/about');
+// });
+
+/* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('./pages/about');
+  res.render('index', { title: 'About' });
 });
 
 /* GET Products page. */
+// router.get('/projects', function(req, res, next) {
+//   res.render('./pages/projects');
+// });
+
+/* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('./pages/projects');
+  res.render('index', { title: 'Projects' });
 });
 
 /* GET Services page. */
+// router.get('/services', function(req, res, next) {
+//   res.render('./pages/services');
+// });
+
+/* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('./pages/services');
+  res.render('index', { title: 'Services' });
 });
 
 
 /* GET Contact us page. */
+// router.get('/contact', function(req, res, next) {
+//   res.render('./pages/contact');
+// });
+
+/* GET Contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('./pages/contact');
+  res.render('index', { title: 'contact' });
 });
 
 module.exports = router;
